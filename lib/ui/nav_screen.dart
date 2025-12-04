4ec98cac98d6d8fac0945d7bafbd39029f66241a
Shitanshu Sajal <singhshitanshu.0206@gmail.com>
2025-12-04 17:01:03 +0530

    Nav Bar Added

---
+++ lib/ui/nav_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:food_cuisine_app/models/meals.dart';
+import 'package:food_cuisine_app/ui/categories.dart';
+import 'meals_screen.dart';
+
+class NavBarScreen extends StatefulWidget {
+  const NavBarScreen({super.key});
+
+  @override
+  State<NavBarScreen> createState() => _NavBarScreenState();
+}
+
+class _NavBarScreenState extends State<NavBarScreen> {
+  final List<Meal> _favouriteMeal = [];
+  int currentTab = 0;
+  void _toggleFavouriteStae(Meal meal) {
+    final isMealAvailable = _favouriteMeal.contains(meal);
+
+    if (isMealAvailable) {
+      setState(() {
+        _favouriteMeal.remove(meal);
+      });
+    } else {
+      setState(() {
+        _favouriteMeal.add(meal);
+      });
+    }
+  }
+
+  void indexCounter(int index) {
+    setState(() {
+      currentTab = index;
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    String appBarTitle = 'Categories';
+    Widget activeWidget = CategoriesScreen();
+
+    if (currentTab == 1) {
+      activeWidget =
+          MealsScreen(title: 'Favourites', categoryMeals: _favouriteMeal);
+      appBarTitle= 'Favourites';
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          appBarTitle,
+        ),
+      ),
+      body: activeWidget,
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: currentTab,
+          onTap: (index) {
+            indexCounter(index);
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.set_meal,
+              ),
+              label: 'Categories',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.star,
+              ),
+              label: 'Favourites',
+            )
+          ]),
+    );
+  }
+}
